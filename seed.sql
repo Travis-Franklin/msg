896366297b0098a3a285034827a07bbb6aebab30
Travis-Franklin <travis.william.franklin@gmail.com>
2020-07-21 22:59:09 -0400

    Adds the skeleton framework for a seed.sql file

---
+++ seed.sql
@@ -0,0 +1,13 @@
+  
+insert into users
+    (first_name, last_name, organization_name, email, phone_number, user_name, password)
+VALUES 
+    ('Creed', 'Bratton', 'Dunder Mifflin', 'creed@dundermifflin.com', '999-999-9999', 'ccreed', 'creed');
+
+insert into events
+    (event_name, event_location, event_date, event_time, event_description, user_id)
+VALUES
+    ('Tree Planting ', 'Piedmont Park ', '2020-04-13', '08:00 AM', 'help preserve our urban tree canopy and plant trees at the park!', 1);
+    ('Teach Coding Basic to Kids', 'Ponce de Leon Library', '2020-01-31', '09:00 AM', 'help teach kids coding skills ', 1);
+    ('Walk Shelter Dogs', 'Atlanta Beltline ', '2020-03-10', '12:00 PM', 'walk dogs available for adoption from Fulton County Animal Services', 1);
+    ('Neighborhood Electronic Recycling', 'Ponce de Leon Library', '2020-01-31', '09:00 AM', 'help get electronic out of our landfills!', 1);
