23c19d15e0bb3be3a20807db5ffeaef528e5f5e8
Travis-Franklin <travis.william.franklin@gmail.com>
2020-07-21 22:57:22 -0400

    Adds comments for next steps in schema build out

--- schema.sql
+++ schema.sql
@@ -7,6 +7,11 @@ create TABLE users (
 );
 
 create TABLE msgs (
-    msg_id serial PRIMARY key
+    msg_id serial PRIMARY key,
     user_id INTEGER REFERENCES users(user_id) NOT NULL,
 );
+
+
+-- Next we will need a table that will contain the msg content which will
+--have to have a msg_id and the content, but also be able to house the responses
+--do we need another linking table?
