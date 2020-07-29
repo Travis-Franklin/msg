de6acb6bd1d9c2137bb104d81d1c4db78379fedb
Travis-Franklin <travis.william.franklin@gmail.com>
2020-07-28 23:06:24 -0400

    Adds try catch to listMsgs function

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const http = require('http');
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
-
-const db = require('./models/connection')
-
 const app = express();
 const server = http.createServer(app);
-
 const PORT = 9000;
+const db = require('./models/connection')
 
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
 async function listMsgs(){
-    const allMsgs = await db.any(`
-    select * from msgs`);
-    console.log(allMsgs);
+    try{
+        console.log('inside try')
+        const allMsgs = await db.any(`
+        select * from msgs`);
+        console.log(allMsgs);
+    } catch (err) {
+        console.log('There is an error inside listMsgs')
+    };
 }
 
 listMsgs();
