2d98d749e011ea62df86e8e4b96a89f7f0135070
Travis-Franklin <travis.william.franklin@gmail.com>
2020-08-12 18:10:24 -0400

    Adds to index.js

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const db = require('./models/connection')
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
-
-async function listMsgs(){
-    try{
-        console.log('inside try')
-        const allMsgs = await db.any(`
-        select * from msgs`);
-        console.log(allMsgs);
-    } catch (err) {
-        console.log('There is an error inside listMsgs')
-    };
-}
-
-listMsgs();
-
 async function insertMsgs(){
     try {
         console.log('inside insertMsgs');
@@ -38,6 +24,20 @@ async function insertMsgs(){
 }
 
 insertMsgs();
+
+async function listMsgs(){
+    try{
+        console.log('inside try')
+        const allMsgs = await db.any(`
+        select * from msgs`);
+        console.log(allMsgs);
+    } catch (err) {
+        console.log('There is an error inside listMsgs')
+    };
+}
+
+listMsgs();
+
 
 server.listen(PORT, () => {
     console.log('im listening');
